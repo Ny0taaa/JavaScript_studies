0bc36ed23ad0c01dc18242230cb29c5d009a0cde
Ny0taaa <davicandidope@gmail.com>
2024-05-02 18:18:47 -0300

    window

--- Pixel Art generator/index.js
+++ Pixel Art generator/index.js
@@ -112,3 +112,7 @@ gridHeight.addEventListener("input", () =>{
     heightValue.innerHTML = gridHeight.value < 9 ? '0${gridHeight.value}' : gridHeight.value;
 });
 
+window.onload = () => {
+    gridWidth.value = 0;
+    gridHeight.value = 0;
+};
