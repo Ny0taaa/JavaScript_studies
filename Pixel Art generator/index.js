71e76504980e13bf88a8d112e91784de5da42387
Ny0taaa <davicandidope@gmail.com>
2024-05-02 18:14:34 -0300

    buttons

--- Pixel Art generator/index.js
+++ Pixel Art generator/index.js
@@ -92,3 +92,23 @@ function checker(elementId){
     });
 }
 
+clearGridButton.addEventListener("click", ()=>{
+    container.innerHTML = "";
+});
+
+eraseBtn.addEventListener("click", ()=>{
+    erase = true;
+});
+
+paintBtn.addEventListener("click", ()=>{
+    erase = false;
+});
+
+gridWidth.addEventListener("input", () =>{
+    widthValue.innerHTML = gridWidth.value < 9 ? '0${gridWidth.value}' : gridWidth.value;
+});
+
+gridHeight.addEventListener("input", () =>{
+    heightValue.innerHTML = gridHeight.value < 9 ? '0${gridHeight.value}' : gridHeight.value;
+});
+
