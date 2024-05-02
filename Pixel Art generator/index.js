8d3eef2c3db55cf84604bc09ac0dce3f84a0ff9a
Ny0taaa <davicandidope@gmail.com>
2024-05-02 13:14:29 -0300

    index.js add

---
+++ Pixel Art generator/index.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container");
+let gridButton = document.getElementById("submit-grid");
+let clearGridButton = document.getElementById("clear-grid");
+let gridWidth = documnet.getElementById("width-range");
+let gridHeight = document.getElementById("height-range");
+let colorButton = document.getElementById("color-input");
+let eraseBtn = document.getElementById("erase-btn");
+let paintBtn = document.getElementById("paint-btn");
+let widtgValue = document.getElementById("width-value");
+let heightValue = document.getElementById("height-value");
+
