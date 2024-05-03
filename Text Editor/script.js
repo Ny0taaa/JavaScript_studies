c547703975960861d426f25891d2fb3df2bac2a1
Ny0taaa <davicandidope@gmail.com>
2024-05-03 17:28:33 -0300

    script.js

---
+++ Text Editor/script.js
@@ -0,0 +1,20 @@
+let optionButtons = documment.querySelectorAll(".option-button");
+let advancedOptionButton = document.querySelectorAll("adv-option-button");
+let fontName = document.getElementById("fontName");
+let fontSizeRef= document.getElementById("fontSize");
+let writingAre = document.getElementById("text-input");
+let linkButton = document.getElementById("creatLink");
+let alignButtons = document.querySelectorAll(".align");
+let spacingButtons = document.querySelectorAll(".spacing");
+let formatButton = document.querySelectorAll(".format");
+let scriptButtons = document.querySelectorAll(".script");
+
+let fontList = [
+    "Arial",
+    "Verdana",
+    "Times New Roman",
+    "Garamond",
+    "Georgia",
+    "Courier New",
+    "Cursive",
+];
